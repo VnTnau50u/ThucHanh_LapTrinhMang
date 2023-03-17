c6f21521cf8fd357ef5e27e04b493c6f6dd4b5fc
HLF <lamlethanh987@gmail.com>
2023-03-17 21:47:25 +0700

    Update Lab1_Bai1.Designer.cs

--- Lab1/Lab1_Bai1.Designer.cs
+++ Lab1/Lab1_Bai1.Designer.cs
@@ -44,9 +44,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Microsoft Sans Serif", 10.5F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(20, 9);
+            label1.Location = new Point(18, 7);
             label1.Name = "label1";
-            label1.Size = new Size(259, 22);
+            label1.Size = new Size(205, 17);
             label1.TabIndex = 0;
             label1.Text = "TÍNH TỔNG 2 SÔ NGUYÊN";
             label1.Click += label1_Click;
@@ -55,9 +55,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Microsoft Sans Serif", 8F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(9, 64);
+            label2.Location = new Point(8, 48);
             label2.Name = "label2";
-            label2.Size = new Size(77, 17);
+            label2.Size = new Size(59, 13);
             label2.TabIndex = 1;
             label2.Text = "Nhập số 1:";
             // 
@@ -65,9 +65,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Microsoft Sans Serif", 8F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(8, 109);
+            label3.Location = new Point(7, 82);
             label3.Name = "label3";
-            label3.Size = new Size(77, 17);
+            label3.Size = new Size(59, 13);
             label3.TabIndex = 2;
             label3.Text = "Nhập số 2:";
             label3.Click += label3_Click;
@@ -76,44 +76,40 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Microsoft Sans Serif", 8F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(20, 200);
+            label4.Location = new Point(18, 172);
             label4.Name = "label4";
-            label4.Size = new Size(45, 17);
+            label4.Size = new Size(35, 13);
             label4.TabIndex = 3;
             label4.Text = "Tổng:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(179, 58);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Location = new Point(157, 44);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 27);
+            textBox1.Size = new Size(88, 23);
             textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(179, 103);
-            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Location = new Point(157, 77);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 27);
+            textBox2.Size = new Size(88, 23);
             textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(179, 200);
-            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Location = new Point(157, 168);
             textBox3.Name = "textBox3";
             textBox3.ReadOnly = true;
-            textBox3.Size = new Size(100, 27);
+            textBox3.Size = new Size(88, 23);
             textBox3.TabIndex = 6;
             // 
             // button_del
             // 
             button_del.Font = new Font("Microsoft Sans Serif", 8F, FontStyle.Regular, GraphicsUnit.Point);
-            button_del.Location = new Point(20, 150);
-            button_del.Margin = new Padding(3, 4, 3, 4);
+            button_del.Location = new Point(18, 126);
             button_del.Name = "button_del";
-            button_del.Size = new Size(75, 29);
+            button_del.Size = new Size(66, 22);
             button_del.TabIndex = 7;
             button_del.Text = "Xóa";
             button_del.UseVisualStyleBackColor = true;
@@ -122,10 +118,9 @@
             // button_sum
             // 
             button_sum.Font = new Font("Microsoft Sans Serif", 8F, FontStyle.Regular, GraphicsUnit.Point);
-            button_sum.Location = new Point(189, 150);
-            button_sum.Margin = new Padding(3, 4, 3, 4);
+            button_sum.Location = new Point(157, 126);
             button_sum.Name = "button_sum";
-            button_sum.Size = new Size(75, 29);
+            button_sum.Size = new Size(66, 22);
             button_sum.TabIndex = 8;
             button_sum.Text = "Tính";
             button_sum.UseVisualStyleBackColor = true;
@@ -134,10 +129,9 @@
             // button_exit
             // 
             button_exit.Font = new Font("Microsoft Sans Serif", 8F, FontStyle.Regular, GraphicsUnit.Point);
-            button_exit.Location = new Point(218, 288);
-            button_exit.Margin = new Padding(3, 4, 3, 4);
+            button_exit.Location = new Point(191, 216);
             button_exit.Name = "button_exit";
-            button_exit.Size = new Size(75, 29);
+            button_exit.Size = new Size(66, 22);
             button_exit.TabIndex = 9;
             button_exit.Text = "Thoát";
             button_exit.UseVisualStyleBackColor = true;
@@ -146,10 +140,10 @@
             // Lab1_Bai1
             // 
             AcceptButton = button_sum;
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             CancelButton = button_exit;
-            ClientSize = new Size(294, 319);
+            ClientSize = new Size(257, 239);
             Controls.Add(button_exit);
             Controls.Add(button_sum);
             Controls.Add(button_del);
@@ -160,7 +154,6 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Lab1_Bai1";
             Text = "Lab1_Bai1";
             Load += Lab1_Bai1_Load;
