1519381bf3a5014b8c41fd8d1affd7fefff977b8
HLF <lamlethanh987@gmail.com>
2023-04-09 05:27:39 +0700

    Bai5

--- Lab2/Lab2_Bai5.Designer.cs
+++ Lab2/Lab2_Bai5.Designer.cs
@@ -29,21 +29,61 @@
         private void InitializeComponent()
         {
             listView1 = new ListView();
+            Cl_name = new ColumnHeader();
+            Cl_size = new ColumnHeader();
+            Cl_extend = new ColumnHeader();
+            Cl_date = new ColumnHeader();
+            button1 = new Button();
             SuspendLayout();
             // 
             // listView1
             // 
-            listView1.Location = new Point(40, 42);
+            listView1.Columns.AddRange(new ColumnHeader[] { Cl_name, Cl_size, Cl_extend, Cl_date });
+            listView1.GridLines = true;
+            listView1.Location = new Point(-1, 55);
             listView1.Name = "listView1";
-            listView1.Size = new Size(705, 367);
+            listView1.Size = new Size(886, 367);
             listView1.TabIndex = 0;
             listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // Cl_name
+            // 
+            Cl_name.Text = "Tên";
+            Cl_name.Width = 167;
+            // 
+            // Cl_size
+            // 
+            Cl_size.Text = "Kích Thước";
+            Cl_size.Width = 241;
+            // 
+            // Cl_extend
+            // 
+            Cl_extend.Text = "Đuôi mở rộng";
+            Cl_extend.Width = 225;
+            // 
+            // Cl_date
+            // 
+            Cl_date.Text = "Ngày tạo";
+            Cl_date.Width = 250;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(382, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Open Folder";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Lab2_Bai5
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(886, 424);
+            Controls.Add(button1);
             Controls.Add(listView1);
             Name = "Lab2_Bai5";
             Text = "Lab2_Bai5";
@@ -53,5 +93,10 @@
         #endregion
 
         private ListView listView1;
+        private ColumnHeader Cl_name;
+        private ColumnHeader Cl_size;
+        private ColumnHeader Cl_extend;
+        private ColumnHeader Cl_date;
+        private Button button1;
     }
 }
--- Lab2/Lab2_Bai5.cs
+++ Lab2/Lab2_Bai5.cs
@@ -16,5 +16,45 @@ namespace Lab2
         {
             InitializeComponent();
         }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            FolderBrowserDialog opf = new FolderBrowserDialog();
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+
+                string[] pathList = Directory.GetFiles(opf.SelectedPath, "*", SearchOption.AllDirectories);
+                List<FileInfo> ListFileInfo = new List<FileInfo>();
+
+                foreach (string item in pathList)
+                {
+                    ListFileInfo.Add(new FileInfo(item));
+                }
+
+
+                foreach (FileInfo fi in ListFileInfo)
+                {
+                    ListViewItem item = new ListViewItem
+                    {
+                        Text = fi.Name
+                    };
+                    listView1.Items.Add(item);
+
+                    ListViewItem.ListViewSubItem kichthuoc = new ListViewItem.ListViewSubItem(item, ((fi.Length)).ToString());
+                    ListViewItem.ListViewSubItem DuoiMoRong = new ListViewItem.ListViewSubItem(item, ((fi.Extension)).ToString());
+                    ListViewItem.ListViewSubItem NgayTao = new ListViewItem.ListViewSubItem(item, ((fi.CreationTime)).ToString());
+                    item.SubItems.Add(kichthuoc);
+                    item.SubItems.Add(DuoiMoRong);
+                    item.SubItems.Add(NgayTao);
+
+                }
+            }
+        }
     }
 }
